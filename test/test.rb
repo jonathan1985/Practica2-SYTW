0e416f3c91c13b9d4c4e10b09381f2cc65daf71d
jonathan1985 <alu0100450187@ull.edu.es>
2014-10-03 08:47:20 +0100

    Test Comprobar Css

--- test/test.rb
+++ test/test.rb
@@ -41,7 +41,10 @@ describe "SYTW P2 - page" do
         assert_match "<p><b>SYTW - Práctica 2 Realizada por:</b><h1>-> Daniel Nicolás Fernández del Castillo Salazar</h1><h1>-> Jonathan Barrera Delgado</h1></p>", last_response.body
     end
     
-    
+    #it "test_styles"
+    #    get "/public/css/estilo.css"
+    #   assert last_response.ok?
+    #end
 end
 
 describe "SYTW P2 - Funtions" do
